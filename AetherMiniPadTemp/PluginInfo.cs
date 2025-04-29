48b7b7d07d3bfc9e87e69ff3cd4aa53d8083bd1a
infernogpt <santinobonomolo142@gmail.com>
2025-04-29 11:17:27 -0400

    Update PluginInfo.cs

--- AetherMiniPadTemp/PluginInfo.cs
+++ AetherMiniPadTemp/PluginInfo.cs
@@ -3,7 +3,7 @@
     internal class PluginInfo
     {
         public const string GUID = "org.iidk.gorillatag.menutemplate";
-        public const string Name = "Aether Client";
+        public const string Name = "Bool Client";
         public const string Description = "Created by @goldentrophy with love <3";
         public const string Version = "1.0.0";
     }
